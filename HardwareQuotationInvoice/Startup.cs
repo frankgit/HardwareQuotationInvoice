da7036ea5717280d392fff50ccf1330ce74c37d6
Frank <fh_mail@hotmail.com>
2017-06-27 23:33:34 +0800

    Add project files.

---
+++ HardwareQuotationInvoice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HardwareQuotationInvoice.Startup))]
+namespace HardwareQuotationInvoice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
